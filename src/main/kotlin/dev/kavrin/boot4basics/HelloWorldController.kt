8939fb8855e47c508c3d44cf09ea0c1d1d4c36a6
Mostafa Hosseini <kavrindev@gmail.com>
2025-09-02 16:11:41 +0330

    Feat: Add HelloWorldController with helloWorld endpoint
    This change introduces a new REST controller that provides a simple "Hello World" endpoint.
    
    Key changes include:
    • Created HelloWorldController.kt:
    • Defined a `@RestController` with a `@GetMapping` for "/hello".
    • Implemented `helloWorld` function that returns "Hello World".

---
+++ src/main/kotlin/dev/kavrin/boot4basics/HelloWorldController.kt
@@ -0,0 +1,13 @@
+package dev.kavrin.boot4basics
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/hello")
+    fun helloWorld(): String {
+        return "Hello World"
+    }
+}
